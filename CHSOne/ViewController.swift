7f9939a9e4723d5ad5f7bf368ce95a1ae439bc35
Hardik Ajmeri <princehp88@gmail.com>
2018-02-22 20:54:37 +0530

    Initial Commit

---
+++ CHSOne/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CHSOne
+//
+//  Created by Hardik Ajmeri on 22/02/18.
+//  Copyright © 2018 FutureScape. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
